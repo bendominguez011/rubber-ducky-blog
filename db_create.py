4481e33d7246747ca944b29b93421d4f4dbb6c7b
Benjamin Dominguez <bendominguez011@gmail.com>
2015-11-29 23:13:00 -0500

    changed db_create.py

--- db_create.py
+++ db_create.py
@@ -1,4 +1,7 @@
 from app import db, Post
 
-db.session.add(Post('Introduction to Flask', 'parsed_flask.html'))
+db.session.rollback()
+db.create_all()
+post = Post("Introduction to Flask Part 1", "parsed_flask.html")
+db.session.add(post)
 db.session.commit()
